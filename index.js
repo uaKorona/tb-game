720e8be4cfd048ef088c17f31bf635dab084ff1a
Roman Kovalskyi <rkovalskiy@deloitte.com>
2023-05-30 23:20:39 +0300

    SPA on ROOT-DOMAIN api endpoinds on ROOT-DOMAIN/api

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const http = require('http');
-
 const app = express();
-const server = http.createServer(app);
 // const io = socketIO(server);
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+// Serve static files for the SPA
+app.use(express.static('public'));
+
+// API routes
+app.get('/api', (req, res) => {
+    res.json({ message: 'API endpoint' });
 });
 
-server.listen(3000, () => {
-    console.log('Server started on port 3000');
+// Serve the SPA for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Start the server on port 80
+const server = app.listen(80, () => {
+    console.log('Server started on port 80');
 });
